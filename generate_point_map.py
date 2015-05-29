be9c51876ab405e0bb350e2c4f3662737fa0299e
Dan Tasse <dan.tasse@gmail.com>
2015-05-29 16:09:30 -0700

    well wrote most of the code, it's a bit of a mess maybe

--- generate_point_map.py
+++ generate_point_map.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python
 
+# Makes a JSON of lat, lon -> neighborhood name. For easier classification of
+# neighborhood - you don't have to figure out which neighborhood each point is
+# in, just round it off to 3 decimal places and then look it up in the
+# point map.
+
+#-123.0137, 37.6040, -122.3549, 37.8324
+
+import argparse, numpy, csv
+#import util.util, util.neighborhoods, util.census
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--granularity', '-g', type=float, default=.001)
+    parser.add_argument('--outfile', '-o', default='point_map.csv')
+    args = parser.parse_args()
+
+    nghds = get_nghd.load_nghds('neighborhoods/neighborhoods.json')
+
+    writer = csv.DictWriter(open(args.outfile, 'w'), ['lat', 'lon', 'nghd'])
+#        'tract', 'block_group', 'block'])
+    writer.writeheader()
+
+    counter = 0
+    for lat in numpy.arange(37.6040, 37.8324, args.granularity):
+        for lon in numpy.arange(-123.0137, -122.3549, args.granularity):
+            lat = round(lat, 3)
+            lon = round(lon, 3)
+            nghd = get_nghd.get_neighborhood_name(nghds, lon, lat)
+#            tract = util.census.get_tract_name(lat, lon)
+#            group = util.census.get_group_ID(lat, lon)
+#            block = util.census.get_block_name(lat, lon)
+#            writer.writerow({'lat': lat, 'lon': lon, 'nghd': nghd,
+#                'tract': tract, 'block_group': group, 'block': block})
+            writer.writerow({'lat': lat, 'lon': lon, 'nghd': nghd})
+            counter += 1
+            if counter % 1000 == 0:
+                print counter
+
---
+++ get_nghd.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import shapely, geojson
+
+#pittsburgh_outline = None
+#nghds = []
+
+def load_nghds(neighborhoods_filename):
+    neighborhoods = geojson.load(open(neighborhoods_filename))
+    nghds = neighborhoods['features']
+    for nghd in nghds:
+        nghd['shape'] = shapely.geometry.asShape(nghd.geometry)
+#    global pittsburgh_outline
+#    pittsburgh_outline = nghds[0]['shape']
+#    for nghd in nghds:
+#        pittsburgh_outline = pittsburgh_outline.union(nghd['shape'])
+    return nghds
+
+def get_neighborhood_name(nghds, lon, lat):
+#    global pittsburgh_outline # TODO ugh globals
+#    if pittsburgh_outline == None:
+#        pittsburgh_outline = nghds[0]['shape']
+#        for nghd in nghds:
+#            pittsburgh_outline = pittsburgh_outline.union(nghd['shape'])
+         
+    point = shapely.geometry.Point(lon, lat)
+#    if not pittsburgh_outline.contains(point):
+#        return 'Outside San Francisco'
+    
+    for nghd in nghds:
+        if nghd['shape'].contains(point):
+            # Move this nghd to the front of the queue so it's checked first next time
+            nghds.remove(nghd)
+            nghds.insert(0, nghd)
+            return nghd.properties['neighborho'] # not a typo.
+    return 'Outside San Francisco'
