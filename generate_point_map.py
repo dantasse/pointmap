5957dcbd3906d6acc4368d4022aa6d73894a6fef
Dan Tasse <dan.tasse@gmail.com>
2016-10-06 16:37:51 -0400

    parallel version works now

--- generate_point_map.py
+++ generate_point_map.py
@@ -8,10 +8,14 @@
 # The map will only go as far as the bounding box of all the neighborhoods. If
 # you're outside that bounding box, you're outside the city anyway.
 
-import argparse, numpy, csv, get_nghd, geojson, shapely, shapely.geometry
-import shapely.ops
+import argparse, numpy, csv, geojson, shapely, shapely.geometry
+import shapely.ops, multiprocessing, time
 
-def get_nghd_name(nghds, lat, lon):
+def init_worker(args):
+    nghds = args[0] # Each process gets a version of the nghds to use later.
+
+def get_nghd_name(args):
+    lat, lon = args
     point = shapely.geometry.Point(lon, lat)
     for nghd in nghds:
         bounds = nghd['bounds']
@@ -22,19 +26,21 @@ def get_nghd_name(nghds, lat, lon):
         if point.within(nghd['shape']):
             # nghds.remove(nghd) # future optimization if necessary
             # nghds.insert(0, nghd)
-            return nghd['properties']['hood'] # For pgh_neighborhoods.geojson only.
-    return 'None'
+            return [lat, lon, nghd['properties']['hood']]
+            # 'hood' is for pgh_neighborhoods.geojson only.
+    return [lat, lon, 'None']
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--granularity', '-g', type=float, help="what to round everything to. default=.001.", default=.001)
-    parser.add_argument('--output_file', '-o', default='point_map.csv')
     parser.add_argument('--neighborhoods_file', default='neighborhoods/pgh_neighborhoods.geojson')
+    parser.add_argument('--num_processes', type=int, default=multiprocessing.cpu_count())
+    parser.add_argument('--output_file', '-o', default='point_map.csv')
     args = parser.parse_args()
 
-    writer = csv.DictWriter(open(args.output_file, 'w'), ['lat', 'lon', 'nghd', 'tract'])
-    writer.writeheader()
+    writer = csv.writer(open(args.output_file, 'w'))
+    writer.writerow(['lat', 'lon', 'nghd', 'tract'])
 
+    print "%s\tReading neighborhoods file." % time.asctime()
     neighborhoods = geojson.load(open(args.neighborhoods_file))
     nghds = neighborhoods['features']
     for nghd in nghds:
@@ -43,17 +49,21 @@ if __name__ == '__main__':
     overall_shape = shapely.ops.cascaded_union([n['shape'] for n in nghds])
     (min_lon, min_lat, max_lon, max_lat) = overall_shape.bounds
 
-    num_pts = ((max_lon - min_lon)/args.granularity) * ((max_lat - min_lat)/args.granularity)
-    print "Number of points to calculate: " + str(round(num_pts))
+    num_pts = ((max_lon - min_lon)/.001) * ((max_lat - min_lat)/.001)
+    print "%s\tNumber of points to calculate: %s" % (time.asctime(), str(round(num_pts)))
 
+    worker_pool = multiprocessing.Pool(args.num_processes, init_worker, (nghds,))
     counter = 0
-    for lat in numpy.arange(min_lat, max_lat, args.granularity):
-        for lon in numpy.arange(min_lon, max_lon, args.granularity):
-            lat = round(lat, 3)
-            lon = round(lon, 3)
-            nghd = get_nghd_name(nghds, lat, lon)
-            writer.writerow({'lat': lat, 'lon': lon, 'nghd': nghd})
-            counter += 1
-            if counter % 1000 == 0:
-                print counter
+    coords = []
+    for lat in numpy.arange(min_lat, max_lat, 0.001):
+        for lon in numpy.arange(min_lon, max_lon, 0.001):
+            coords.append((round(lat, 3), round(lon, 3)))
+    
+    results = worker_pool.map(get_nghd_name, coords)
+    # Synchronous map; can't go on until this is done.
 
+    print "%s\tCalculated neighborhoods, writing output." % time.asctime()
+
+    for result in results:
+        writer.writerow(result + [''])
+ 
