714a99cbbd025ee0a06a577d8b3b065071667d1d
Dan Tasse <dan.tasse@gmail.com>
2016-10-06 15:04:48 -0400

    add pgh neighborhoods, move stuff, update readme, add requirements

--- generate_point_map.py
+++ generate_point_map.py
@@ -7,26 +7,35 @@
 
 #-122.518, 37.705, -122.350, 37.834
 
-import argparse, numpy, csv, get_nghd
+import argparse, numpy, csv, get_nghd, geojson, shapely
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--granularity', '-g', type=float, default=.001)
+    parser.add_argument('--granularity', '-g', type=float, help="what to round everything to. default=.001.", default=.001)
     # Defaults are for SF
-    parser.add_argument('--min_lat', type=float, default=37.705)
-    parser.add_argument('--max_lat', type=float, default=37.834)
-    parser.add_argument('--min_lon', type=float, default=-122.518)
-    parser.add_argument('--max_lon', type=float, default=-122.350)
-    parser.add_argument('--outfile', '-o', default='point_map.csv')
+    # parser.add_argument('--min_lat', type=float, default=37.705)
+    # parser.add_argument('--max_lat', type=float, default=37.834)
+    # parser.add_argument('--min_lon', type=float, default=-122.518)
+    # parser.add_argument('--max_lon', type=float, default=-122.350)
+    parser.add_argument('--output_file', '-o', default='point_map.csv')
+    parser.add_argument('--neighborhoods_file', default='neighborhoods/pgh_neighborhoods.json')
+    parser.add_argument('--tracts', default='neighborhoods/pgh_tracts.json')
     args = parser.parse_args()
 
-    nghds = get_nghd.load_nghds(open('neighborhoods/neighborhoods.json'))
-    tracts = get_nghd.load_tracts(open('tracts/tracts.json'))
+    nghds = get_nghd.load_nghds(open(args.neighborhoods_file))
+    tracts = get_nghd.load_tracts(open(args.tracts_file))
 
     writer = csv.DictWriter(open(args.outfile, 'w'), ['lat', 'lon', 'nghd', 'tract'])
-#        'tract', 'block_group', 'block'])
-    writer.writerow({'lat': 'lat', 'lon': 'lon', 'nghd': 'nghd', 'tract': 'tract'})
-    # no writeheader in python 2.6
+    writer.writeheader()
+
+    neighborhoods = geojson.load(neighborhoods_file)
+    nghds = neighborhoods['features']
+    for nghd in nghds:
+        # nghd['shape'] = nghd['geometry']['coordinates'][0]
+        # TODO This will fail on multi-polygons
+        nghd['shape'] = nghd
+    
+
 
     min_lat = args.min_lat
     max_lat = args.max_lat
